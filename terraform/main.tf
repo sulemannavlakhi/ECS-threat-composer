aece7ca410afe056ab6d22cf8d503508c98aa775
Your Name <you@example.com>
2026-03-12 16:10:27 -0400

    module ecs added into main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,20 @@ module "vpc" {
   private_subnet_2                       = var.private_subnet_2
   private_subnet_map_public_ip_on_launch = var.private_subnet_map_public_ip_on_launch
 }
+
+module "ecs" {
+  source = "./modules/ecs"
+
+  vpc_id             = module.vpc.vpc_id
+  subnetprivate1_id  = module.vpc.private_subnet_1_id
+  subnetprivate2_id  = module.vpc.private_subnet_2_id
+  tg_arn             = module.alb.target_group_arn
+
+  application_port   = var.application_port
+  ecs_container_name = var.ecs_container_name
+  ecs_docker_port    = var.ecs_docker_port
+  ecs_memory         = var.ecs_memory
+  ecs_cpu            = var.ecs_cpu
+  ecs_desired_count  = var.ecs_desired_count
+  ecs_image          = var.ecs_image
+}
