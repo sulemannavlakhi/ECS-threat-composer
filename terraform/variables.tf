4b98e6fa4f6ecd8cd65d2a51a59da5f51f49aeae
Your Name <you@example.com>
2026-03-12 16:09:54 -0400

    updated file with ecs module variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,4 @@
+#variables used in vpc module
 variable "routetable_cidr" {
   type        = string
   description = "route table CIDR"
@@ -37,3 +38,72 @@ variable "private_subnet_2" {
   type        = string
   description = "private subnet 2 CIDR"
 }
+
+#variables used in ecs module
+
+variable "application_port" {
+  type        = number
+  description = "port for the application"
+}
+
+variable "ecs_container_name" {
+  type        = string
+  description = "name of the container in the task definition"
+}
+
+variable "tg_arn" {
+  type        = string
+  description = "target group arn from the alb module"
+}
+
+variable "ecs_docker_port" {
+  type        = number
+  description = "port the docker image uses"
+}
+
+variable "ecs_memory" {
+  type        = number
+  description = "memory value for the task definition"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "vpc id from the vpc module"
+}
+
+variable "subnetprivate1_id" {
+  type        = string
+  description = "private1 subnet id from the vpc module"
+}
+
+variable "subnetprivate2_id" {
+  type        = string
+  description = "private2 subnet id from the vpc module"
+}
+
+variable "ecs_image" {
+  type        = string
+  description = "url for the latest docker image"
+}
+
+variable "ecs_cpu" {
+  type        = number
+  description = "cpu value for task definition"
+}
+
+variable "ecs_desired_count" {
+  type        = number
+  description = "number of instances for task definition to be deployed"
+}
+
+variable "ingress_cidr" {
+  type        = string
+  description = "ingress cidr"
+}
+
+variable "egress_cidr" {
+  type        = string
+  description = "egress cidr"
+}
+
+
